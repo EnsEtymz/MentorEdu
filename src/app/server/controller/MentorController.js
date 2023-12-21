940e4df634f4aa8be671f1512f91ff7af7131279
enes <etyimezenes8@gmail.com>
2023-12-21 05:09:22 +0300

    controller işlemleri mysql'e göre güncellendi

--- src/app/server/controller/MentorController.js
+++ src/app/server/controller/MentorController.js
@@ -1,20 +1,42 @@
 const express = require('express')
 const router = express()
 const { connection } = require('../config/sqlDatabase')
+const path = require('path')
 
 router.post('/create', (req, res) => {
-    const { username, title, content, category } = req.body
-
-    connection.query('INSERT INTO edu_table SET ?', { username, title, content, category }, (err, results) => {
-        if (err) {
-            console.error('Veri eklenirken hata oluştu:', err);
-            return res.json({ message: 'veri eklenirken bir hatayla karşılaşıldı!', success: false })
-
+    try {
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).json({ message: 'Dosya seçilmedi', success: false });
         }
-        return res.json({ message: 'veri eklendi', success: true })
-    });
 
-})
+        const { username, title, content, category } = req.body;
+        const uploadedFile = req.files.file;
+        const filePath = `uploads/${uploadedFile.name}`;
+
+        uploadedFile.mv(path.resolve(__dirname, `../../../../public/${filePath}`), (err) => {
+            if (err) {
+                console.error('Dosya kaydedilemedi:', err);
+                return res.status(500).json({ message: 'Dosya kaydedilemedi', success: false });
+            }
+
+            // Dosya başarıyla yüklendi, veritabanına kaydedebilirsiniz
+            connection.query('INSERT INTO edu_table SET ?', { username, title, content, category, image_url: `/uploads/${uploadedFile.name}` }, (dbErr, results) => {
+                if (dbErr) {
+                    console.error('Veri eklenirken hata oluştu:', dbErr);
+                    return res.status(500).json({ message: 'Veri eklenirken bir hata oluştu!', success: false });
+                }
+
+                return res.json({ message: 'Veri eklendi', success: true });
+            });
+        });
+    } catch (error) {
+        console.error('Hata:', error);
+        res.status(500).json({ message: 'Bir hata oluştu', success: false });
+    }
+});
+
+
+
 
 
 router.post('/listid', async (req, res) => {
@@ -31,6 +53,32 @@ router.post('/listid', async (req, res) => {
 
 })
 
+
+
+
+
+router.get('/getById/:id', async (req, res) => {
+    const id_edu = req.params.id;
+    connection.query('SELECT * FROM edu_table WHERE id_edu=?', [id_edu], (err, result) => {
+        if (err) {
+            return res.json({ message: 'veri listelenirken bir hatayla karşılaşıldı!', success: false })
+        }
+        return res.json(result)
+    })
+
+})
+
+router.get('/removeById/:id', async (req, res) => {
+    const id_edu = req.params.id;
+    connection.query('DELETE FROM edu_table WHERE id_edu=?', [id_edu], (err, result) => {
+        if (err) {
+            return res.json({ message: 'veri listelenirken bir hatayla karşılaşıldı!', success: false })
+        }
+        return res.json({ message: 'Başarıyla Silindi.', success: true })
+
+    })
+
+})
 
 
 module.exports = router
--- src/app/server/controller/UserController.js
+++ src/app/server/controller/UserController.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express()
-const UserSchema = require('../model/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const nodemailer = require('nodemailer')
+const { connection } = require('../config/sqlDatabase')
+
 
 
 // Kullanıcı Kayıt (Register) İşlemi
@@ -11,21 +12,34 @@ router.post('/register', async (req, res) => {
 
     try {
         const { email, username, password, role } = req.body
-        const user = await UserSchema.findOne({ email: email })
-
-        if (user) {
-            return res.status(500).json({ message: "Bu email zaten mevcut!", success: false })
-        }
-
         const passwordHash = await bcrypt.hash(password, 12)
+        const emailCheck = 'SELECT COUNT(*) as count from user_table WHERE email=?';
 
-        const newUser = await UserSchema.create({ email, username, password: passwordHash, role })
-        const userToken = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, { expiresIn: '1h' })
-        res.status(201).json({
-            success: true,
-            message: "Kayıt İşlemi Başarılı.",
-            newUser,
-            userToken
+        connection.query(emailCheck, [email], (err, result) => {
+            if (err) {
+                return res.json({ message: 'Email check işlemi esnasında hata oluştu.', success: false })
+            }
+
+            const emailCount = result[0].count;
+
+            if (emailCount > 0) {
+
+                return res.json({ message: 'Bu e-posta adresi zaten kullanımda.', success: false });
+            }
+            else {
+                const registerQuery = 'INSERT INTO user_table (email, username, password, role) VaLUES (?, ?, ?, ?)'
+
+                connection.query(registerQuery, [email, username, passwordHash, role], (err, registerResult) => {
+                    if (err) {
+                        return res.json({ message: 'Kayıt oluştururken bir sorunla karşılaşıldı', err, success: false })
+                    }
+                    const newUserId = registerResult.insertId;
+                    const userToken = jwt.sign({ id: newUserId }, process.env.SECRET_KEY, { expiresIn: '1h' })
+                    return res.json({
+                        message: 'Kayıt işlemi başarılı', success: true, newUser: { _id: newUserId, email, username, role }, userToken
+                    })
+                })
+            }
         })
     }
     catch (error) {
@@ -37,30 +51,39 @@ router.post('/register', async (req, res) => {
 
 //Kullanıcı Giriş (Login) İşlemi
 router.post('/login', async (req, res) => {
-
     try {
-        const { email, password } = req.body
-        const user = await UserSchema.findOne({ email })
-        if (!user) {
-            return res.status(500).json({ message: "Email Bulunamadı!!", success: false })
-        }
-        const comparePassword = await bcrypt.compare(password, user.password)
-        if (!comparePassword) {
-            return res.status(500).json({ message: "Parola yanlış!!", success: false })
-        }
+        const { email, password } = req.body;
 
-        const userToken = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' })
-        res.status(201).json({
-            success: true,
-            message: "Giriş Başarılı",
-            user,
-            userToken
-        })
+        const emailCheck = 'SELECT * from user_table WHERE email=?';
+
+        connection.query(emailCheck, [email], async (err, result) => {
+            if (err) {
+                return res.json({ message: 'Email check işlemi esnasında hata oluştu.', success: false });
+            }
+
+            if (!result[0]) {
+                return res.json({ message: 'Email bulunamadı.', success: false });
+            }
+
+            const comparePassword = await bcrypt.compare(password, result[0].password);
+
+            if (!comparePassword) {
+                return res.status(500).json({ message: "Parola yanlış!!", success: false });
+            }
+
+            const userToken = jwt.sign({ id: result[0]._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+
+            return res.status(201).json({
+                success: true,
+                message: "Giriş Başarılı",
+                user: result[0],
+                userToken
+            });
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+});
 
 
 
@@ -104,17 +127,19 @@ router.post('/forgotpassword', async (req, res) => {
 
 router.post('/resetpassword', async (req, res) => {
     const { email, password } = req.body
+    const passwordHash = await bcrypt.hash(password, 12)
+
     if (password.length < 6) {
-        return res.json({ success: false, message: 'Parola en az 6 karakter olmalıdır.' });
+        res.json({ message: 'Parola 6 haneden küçük olamaz.', success: false })
     } else {
-        const passwordHash = await bcrypt.hash(password, 12)
-        await UserSchema.findOneAndUpdate({ email }, { password: passwordHash })
-            .then(() => {
-                res.json({ success: true, message: "Parola sıfırlama işlemi başarılı." })
-            })
-            .catch(() => {
-                res.json({ success: false, message: "Bir hatayla karşılaşıldı!" })
-            })
+
+        const resetQuery = 'UPDATE user_table SET password=? WHERE email=?'
+        connection.query(resetQuery, [email, passwordHash], (err, result) => {
+            if (err) {
+                return res.json({ message: 'Parola güncelleme esnasında bir hata oluştu', success: false })
+            }
+            return res.json({ message: 'Parola değiştirme işlemi başarılı.', success: true })
+        })
     }
 })
 
